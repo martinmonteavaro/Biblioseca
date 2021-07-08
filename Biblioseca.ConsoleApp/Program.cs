e004a450d80dceccfd2f4bf8ec12d7313b65c9a2
Martin <martinmonteavaro@gmail.com>
2021-07-07 22:12:14 -0300

    Segunda actualizacion Biblioseca

--- Biblioseca.ConsoleApp/Program.cs
+++ Biblioseca.ConsoleApp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Biblioseca.Model;
+using NHibernate;
+using NHibernate.Cfg;
 
 namespace Biblioseca.ConsoleApp
 {
@@ -11,7 +13,20 @@ namespace Biblioseca.ConsoleApp
     {
         static void Main(string[] args)
         {
+            ISessionFactory sessionFactory = new Configuration()
+                .Configure()
+                .BuildSessionFactory();
+
+            ISession session = sessionFactory.OpenSession();
+
             Author author = new Author();
+            author.FirstName = "Steve";
+            author.LastName = "Rogers";
+
+            session.Save(author);
+
+            Console.WriteLine(author.Id);
+            Console.ReadKey();
         }
     }
 }
